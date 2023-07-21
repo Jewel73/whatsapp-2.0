7c9fc63baadffbf0b735cad9c9089673cddc2ac5
jewel73 <jewelranar420@gmail.com>
2023-07-21 11:52:25 +0600

    initialize wp 2.0 backend spring boot project

---
+++ wp-backend/src/main/java/com/jewel/wpbackend/WpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jewel.wpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpBackendApplication.class, args);
+	}
+
+}
---
+++ wp-backend/src/test/java/com/jewel/wpbackend/WpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewel.wpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
